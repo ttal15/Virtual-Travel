a09e20bc9d06ee94adb5509d3d4ccb66876e8201
Kim JaeDoo <ttal15@naver.com>
2015-05-24 14:56:37 +0900

    Added UI R&D

---
+++ Virtual Travel/headControllViewController.h
@@ -0,0 +1,13 @@
+//
+//  headControllViewController.h
+//  Virtual Travel
+//
+//  Created by Steve-Mac on 2015. 5. 24..
+//  Copyright (c) 2015년 InciteStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface headControllViewController : UIViewController
+
+@end
---
+++ Virtual Travel/headControllViewController.m
@@ -0,0 +1,37 @@
+//
+//  headControllViewController.m
+//  Virtual Travel
+//
+//  Created by Steve-Mac on 2015. 5. 24..
+//  Copyright (c) 2015년 InciteStudio. All rights reserved.
+//
+
+#import "headControllViewController.h"
+
+@interface headControllViewController ()
+
+@end
+
+@implementation headControllViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
