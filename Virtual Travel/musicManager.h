3fd6bb1707fe2fd24e8144655f7bc205716f8a10
Kim JaeDoo <ttal15@naver.com>
2015-05-30 12:59:49 +0900

    added ble model

---
+++ Virtual Travel/musicManager.h
@@ -0,0 +1,13 @@
+//
+//  musicManager.h
+//  Virtual Travel
+//
+//  Created by Steve-Mac on 2015. 5. 27..
+//  Copyright (c) 2015년 InciteStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface musicManager : NSObject
+
+@end
---
+++ Virtual Travel/musicManager.m
@@ -0,0 +1,13 @@
+//
+//  musicManager.m
+//  Virtual Travel
+//
+//  Created by Steve-Mac on 2015. 5. 27..
+//  Copyright (c) 2015년 InciteStudio. All rights reserved.
+//
+
+#import "musicManager.h"
+
+@implementation musicManager
+
+@end
